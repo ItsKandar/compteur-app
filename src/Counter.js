1791d5eb5f3e69f3a97c2e3d7c3927f83969b808
ItsKandar <nmoreau15@myges.fr>
2023-11-24 11:51:31 +0100

    modifs

--- src/Counter.js
+++ src/Counter.js
@@ -1,6 +1,4 @@
-import { useState } from "react";
-
-function Counter({count, onClickMinus, OnClickPlus, errorMessage, setIsError}) {
+function Counter({count, onClickMinus, OnClickPlus, errorMessage}) {
 
   return (
     <div className="App">
